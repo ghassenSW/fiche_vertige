2f262711199f1d1c139932bb4d2b4d182313cacc
ghassenSW <155910115+ghassenSW@users.noreply.github.com>
2024-12-24 10:23:37 +0100

    'commit'

---
+++ fiche_vertige_enfants/enfants_app.js
@@ -0,0 +1,54 @@
+const checkboxes = document.querySelectorAll('.other_check');
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener('change', function () {
+
+    checkboxes.forEach((box) => {
+      const targetId = box.getAttribute('data-target');
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.classList.add('hidden');
+      }
+    });
+    const targetId = this.getAttribute('data-target');
+    const targetElement = document.getElementById(targetId);
+    if (this.checked && targetElement) {
+      targetElement.classList.remove('hidden');
+    }
+  });
+});
+
+function updateMainChoice(otherChoiceInput,id) {
+  const mainChoice = document.getElementById(id);
+  console.log(otherChoiceInput,mainChoice)
+  if (otherChoiceInput.trim()) {
+    mainChoice.value = otherChoiceInput;
+  }
+}
+
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('dynamicForm');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+    const names=new Set()
+    Array.from(form.elements).forEach((element) => {
+      if (element.name) {
+        names.add(element.name)
+      }
+    });
+    const data = new FormData(form);
+    for (let iter of names){
+      const name_tab = data.getAll(iter);
+      const name_string = name_tab.join(', ');
+      data.set(iter, name_string);
+    }
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
